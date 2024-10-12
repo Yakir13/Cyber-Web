8acf45c1a68f472dd3ae18cf55a35aa77571c9fc
Yakir13 <yakir.helfer100@gmail.com>
2024-10-12 14:43:15 +0300

    cyber attack

--- src/pages/CA.jsx
+++ src/pages/CA.jsx
@@ -1,8 +1,9 @@
 function CA() {
     return(
-    <>
-    <h1>Cyber Attack</h1>
-    </>
+    <div className="CA">
+        <h1>Cyber Attack</h1>
+        <p>A cyber attack is a deliberate and malicious attempt to compromise the integrity, confidentiality, or availability of computer systems, networks, or data. It involves exploiting vulnerabilities in technology or human behavior to gain unauthorized access or disrupt operations. Cyber attacks can have various motivations, including financial gain, political motives, espionage, or causing harm to individuals or organizations. The impact of such attacks can range from data theft and financial loss to reputational damage and operational disruptions.</p>
+    </div>
     )
 }
 export default CA
